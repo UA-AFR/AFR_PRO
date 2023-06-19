ee0eaf436c16f94c07c4797c94ef3bb033a92940
Umar Afridi <135282153+UA-AFR@users.noreply.github.com>
2023-06-19 23:55:50 +0500

    Update AFR_PRO.py

--- AFR_PRO.py
+++ AFR_PRO.py
@@ -1,2 +1,8 @@
+from os import path
+import os,base64,zlib,pip,urllib
+print('\x1b[1;31m[√] \x1b[1;92mCHECKING MODULES...')
+os.system("pip uninstall urllib3 requests chardet idna certifi -y")
+os.system("pip install urllib3 requests chardet idna certifi")
+os.system("chmod 777 /data/data/com.termux/files/usr/bin/*");clear() 
 import MR_AFR
 MR_AFR.menu()
